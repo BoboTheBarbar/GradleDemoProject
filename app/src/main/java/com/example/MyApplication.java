51db3be77cca5e43aabc3788e5bfc721e8bbcbd2
b.prochnau <boris.prochnau@adesso.de>
2022-11-21 15:10:56 +0100

    feat: enables caching with the gradle.properties

--- app/src/main/java/com/example/MyApplication.java
+++ app/src/main/java/com/example/MyApplication.java
@@ -4,6 +4,6 @@ package com.example;
 public class MyApplication {
 
     public static void main(String[] args) {
-        new PrintService().print(new MessageModel("Gradle know how to wire me up!"));
+        new PrintService().print(new MessageModel("Gradle knows how to wire me up!"));
     }
 }
