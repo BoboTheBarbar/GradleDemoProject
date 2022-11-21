53c73fb8964b3bc010e0b686ee67865a67b9687c
b.prochnau <boris.prochnau@adesso.de>
2022-11-21 17:43:38 +0100

    feat: modify compileJava & compileJavaTest Task to configure UTF-8 encoding

--- gradle/plugins/my-java-plugins/src/main/kotlin/my-java-base.gradle.kts
+++ gradle/plugins/my-java-plugins/src/main/kotlin/my-java-base.gradle.kts
@@ -6,3 +6,11 @@ plugins {
 java {
     toolchain.languageVersion.set(JavaLanguageVersion.of(11))
 }
+
+tasks.compileJava {
+    options.encoding = "UTF-8"
+}
+
+tasks.compileTestJava {
+    options.encoding = "UTF-8"
+}
